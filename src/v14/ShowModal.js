0a958c87bb10f6c21c3348fb0a3a31986cd7728a
Mateie <mateie@email.com>
2022-03-19 00:13:51 -0700

    fixed things i guess

---
+++ src/v14/ShowModal.js
@@ -0,0 +1,64 @@
+const { Client, Interaction } = require('discord.js');
+const { InteractionResponseTypes } = require('../util/Constants');
+const Modal = require('../structures/Modal');
+const { Error } = require('../structures/errors');
+const { Routes } = require('discord-api-types/v10');
+
+/**
+ * Shows the Modal to the Interaction User.
+ * @param {Modal} Modal Modal.
+ * @param {Object} Options Client and Interaction.
+ * @example
+ * showModal(Modal, {
+ *   client: Client, // Client to show the Modal through the Discord API.
+ *   interaction: Interaction // Interaction data.
+ * });
+ * @returns {Modal} Modal.
+*/
+
+async function showModal(modal, options){
+
+  if (!modal) throw new Error('MODAL_REQUIRED');
+  if (!options) throw new Error('OPTIONS_REQUIRED');
+  if (!options.client) throw new Error('CLIENT_REQUIRED');
+  if (!options.interaction) throw new Error('INTERACTION_REQUIRED');
+  if (!(options.client instanceof Client)) throw new Error('INVALID_CLIENT');
+  if (!(options.interaction instanceof Interaction)) throw new Error('INVALID_INTERACTION');
+
+  let _modal = modal instanceof Modal ? modal : null
+
+  function isJSONModal(modal) {
+    if(!_modal && typeof modal === 'object' && modal.title && modal.custom_id && modal.components){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if(!isJSONModal(modal) && !(modal instanceof Modal)) throw new Error('INVALID_MODAL');
+
+  switch(isJSONModal(modal)) {
+    case true:
+      _modal = _modal
+      break;
+    case false:
+      _modal = _modal.toJSON()
+      break;
+  }
+
+  try{
+    await options.client.rest.post(Routes.interactionCallback(options.interaction.id, options.interaction.token), {
+      body: {
+        type: InteractionResponseTypes.MODAL,
+        data: _modal,
+      },
+    });
+  } catch(error) {
+      console.error('SHOW_MODAL_ERROR: An error occurred when showing a modal.', error);
+  }
+
+  return new Modal(modal, options.client);    
+    
+}
+
+module.exports = showModal;
